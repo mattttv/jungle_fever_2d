eb10cd940532a1a4fda1cb40d92afe10db552188
Matthias Vierthaler <matthias.vierthaler@student.tugraz.at>
2014-05-29 15:57:02 +0200

    fullscreen not really but it increases size

--- www/gamecontroller.js
+++ www/gamecontroller.js
@@ -1,22 +1,36 @@
 
 function doGameController() {
 
-			if (upKey.isDown)
-            {
-                sprite.y--;
+	if (upKey.isDown)
+		{
+			sprite.y--;
                 // console.log("down");
-            }
-            else if (downKey.isDown)
-            {
-                sprite.y++;
-            }
+        }
+    else if (downKey.isDown)
+    {
+    	sprite.y++;
+    }
 
-            if (leftKey.isDown)
-            {
-                sprite.x--;
-            }
-            else if (rightKey.isDown)
-            {
-                sprite.x++;
-            }
+    if (leftKey.isDown)
+    {
+    	sprite.x--;
+    }
+    else if (rightKey.isDown)
+    {
+
+    	sprite.x++;
+    }
+    else if (fullScreenKey.isDown){
+    	console.log("full screen");
+    	//gofull();
+    	requestFullscreen();
+    }
 }
+
+
+function gofull(){
+        	Phaser.StageScaleMode.EXACT_FIT = 0;
+        	game.stage.scale.startFullScreen();
+        	alert('full screen');
+        }
+        
--- www/main.js
+++ www/main.js
@@ -1,17 +1,15 @@
 var sprite;
 window.onload = function() {
 
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', 
+    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', 
       { preload: preload, create: create, update: update  });
 
     function preload () {
-
         game.load.image('logo', 'phaser.png');
-
     }
 
     function create () {
-       
+
         sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
         sprite.anchor.setTo(0.5, 0.5);
 
@@ -19,11 +17,14 @@ window.onload = function() {
         downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
         leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        fullScreenKey =  game.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_0);
 
     }
 
     function update() {
         doGameController();
     }
+
+
 
 };
