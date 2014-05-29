3053886497258f884d7fb4d90c995ba3f7731027
Matthias Vierthaler <matthias.vierthaler@student.tugraz.at>
2014-05-29 21:25:55 +0200

    misc

---
+++ www/misc.js
@@ -0,0 +1,13 @@
+DIRECTION = {
+    UP : 0,
+    DOWN : 1,
+    RIGHT : 2,
+    LEFT : 3
+}
+
+function debugPrint(message) {
+  var debugOutput = true
+  if (debugOutput) {
+  	console.log(message);
+  }
+};
