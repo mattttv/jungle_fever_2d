b58a459bc531e75bd2eed92f00a7e1588a357ff5
zm0 <mf@tt4.at>
2014-05-29 17:22:00 +0200

    resize

--- www/main.js
+++ www/main.js
@@ -5,7 +5,9 @@ var cursors;
 var upKey, downKey, leftKey, rightKey;
 window.onload = function() {
 
-	var game = new Phaser.Game(window.innerWidth, window.innerHeight,
+	var game = new Phaser.Game(
+			400,300,
+			// window.innerWidth, window.innerHeight,
 			Phaser.CANVAS, 
 			'drvoodo', 
 			{ preload: preload, create: create, update: update, render: render });
@@ -28,15 +30,17 @@ window.onload = function() {
 	    plants.enableBody = true;
 
 
-	    for (var i = 0; i < 50; i++)
+	    for (var i = 0; i < 5; i++)
 	    {
-	        var s = plants.create(game.world.randomX, game.world.randomY, 'baddie');
+	        var s = plants.create(
+	        		game.world.randomX, game.world.randomY, 
+	        		'baddie');
 	        s.body.collideWorldBounds = true;
 	        s.body.bounce.set(1);
 	        // s.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
 	    }
 
-	    drvoodo = game.add.sprite(400, 400, 'drvoodo');
+	    drvoodo = game.add.sprite(game.world.randomX, game.world.randomY, 'drvoodo');
 
 	    game.physics.enable(drvoodo, Phaser.Physics.ARCADE);
 
