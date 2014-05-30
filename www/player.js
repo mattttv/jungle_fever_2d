c1b1be5625b280f2bae7eab04c3df01c9d72bd22
ChaosFX <dominik.lingnau@gmail.com>
2014-05-30 20:49:09 +0200

    asdf

--- www/player.js
+++ www/player.js
@@ -1,10 +1,8 @@
-//function setup_player() {
-//        player   = game.add.sprite(32, 0.5, 'dude');
-//
-//        //  Our two animations, walking left and right.
-//        player.animations.add('left', [0, 1, 2, 3], 10, true);
-//        player.animations.add('right', [5, 6, 7, 8], 10, true);
-//
-//        player.animations.add('down', [4], 10, true);    
-//    
-//}    
+Player = function (game, player) {
+    var x = 100;
+    var y = 100;
+    
+    this.game = game;
+    this.plyer = player;
+    
+}
