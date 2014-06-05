9418529ab25fb03f356a7bf2bb52f355c72cfe26
zm0 <mf@tt4.at>
2014-06-05 13:06:45 +0200

    Copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         concat: {
+
+// combine css, , right now there is none
 //            css: {
 //               src: [
 //                     'css/*'
@@ -9,6 +11,9 @@ module.exports = function(grunt) {
 //                dest: 'combined.css'
 //            },
         	
+        //
+        // combine source js into one big file
+        //
             js : {
                 src : [
                     'src/js/*',
@@ -23,33 +28,60 @@ module.exports = function(grunt) {
                 dest : 'www/lib.js'
             }
         },
+
+// minify css, right now there is none
 //        cssmin : {
 //            css:{
 //                src: 'combined.css',
 //                dest: 'combined.min.css'
 //            }
 //        },
+
         uglify : {
             js: {
                 files: {
                     'www/jfeev.min.js' : [ 'build/src-combined.js' ]
                 }
             }
-        }
+        },
+
+	copy : {
+        //
+    	// copy resources from src to www as a means of deployment
+        //
+		resources: {
+            files: [
+                {expand: true, cwd: 'src/resources/', src: ['**'], dest: 'www/resources'}
+            ]},
+        libs: {
+            files: [
+                {expand: true, cwd: 'src/lib/', src:['**'], dest: 'www/lib'}
+            ]}
+    }
+	
 //        watch: {
 //            files: ['css/*', 'js/*'],
 //            tasks: ['concat', 'cssmin', 'uglify']
 //        }
+
+
     });
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.registerTask('default', [
-        'concat:css', 
-        'cssmin:css', 
-        'concat:js', 
-        'uglify:js' 
+        //'concat:css', 
+        //'cssmin:css', 
+        'concat:js',
+        'concat:jslib',  
+
+        'uglify:js',
+
+        'copy:resources',
+        'copy:libs',
+
     ]);
 };
